32b2eabc4797a368e4ead32f2abaa6b801511831
alexjvr1 <alexjvr@gmail.com>
2023-10-21 13:11:24 +0100

    refactor and optimise milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -9,20 +9,20 @@ print(word_list)
 
 import random
 
-word = random.choice(word_list)
-print(word)
+random_word = random.choice(word_list)
+print(random_word)
 
 #User guesses a letter
 
-guess = input("Guess a letter: ")
+user_letter_guess = input("Guess a letter: ")
 
-#Check that the letter is a valid input (one alphabetical guess)
+#Check that the letter is a valid input - alphabetical and a single letter
 import re
 
-if re.match(r"^[a-zA-Z]",guess):
-	if len(guess) == 1:
+if re.match(r"^[a-zA-Z]",user_letter_guess):
+	if len(user_letter_guess) == 1:
 		print("That's a good guess")
-	elif len(guess) > 1: 
+	elif len(user_letter_guess) > 1: 
 		print("Guess one letter at a time")
 else:
 	print("Oops! That is not a valid input.")
