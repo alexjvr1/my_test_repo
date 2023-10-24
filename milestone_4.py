1560784bc7c294f105aea643210e191638e47ebe
alexjvr1 <alexjvr@gmail.com>
2023-10-24 16:25:54 +0100

    Initiate class for Hangman game

---
+++ milestone_4.py
@@ -0,0 +1,25 @@
+""
+A game of Hangman
+
+""
+
+#Modules to import
+import random
+
+
+#Create the Hangman class
+class Hangman: 
+
+""" Hangman Class with all the functions to play Hangman """
+
+
+	#Initialise the attributes of the class
+	def __init__(self, word_list, num_lives=5):
+"""The class requires a list of words (word_list), and number of lives (num_lives, default=5) """
+
+	#define the parameters
+	self.word_list =
+	self.num_lives = num_lives
+
+
+
