3ea4a87d424bfe0576449f926344f33416bef6c6
alexjvr1 <alexjvr@gmail.com>
2023-10-24 20:51:31 +0100

    initialise all the variables for Hangman Class

--- milestone_4.py
+++ milestone_4.py
@@ -1,11 +1,12 @@
 ""
 A game of Hangman
 
+A word is randommy chosen from a list of words
 ""
 
 #Modules to import
 import random
-
+import re
 
 #Create the Hangman class
 class Hangman: 
@@ -18,8 +19,11 @@ class Hangman:
 """The class requires a list of words (word_list), and number of lives (num_lives, default=5) """
 
 	#define the parameters
-	self.word_list =
+	self.word_list = word.list
 	self.num_lives = num_lives
-
+	self.word = random.choice(word_list)
+	self.word_guessed = list(re.sub("[a-zA-Z]", "_", self.word))
+	self.num_letters = self.word_guessed.count("_")
+	self.num_lives = 5
 
 
